fe52efa5e4ef4369d437d99c0c37819ecac92f1a
Shayron Aguiar <shayron.aguiar@gmail.com>
2017-03-20 03:36:25 +0000

    Implementation of functions

--- includes/lista.h
+++ includes/lista.h
@@ -13,16 +13,19 @@ typedef struct {
     int longitude;
 } TLista, *Lista;
 
-static Lista inicList( void );                      // Cria e retorna uma lista vazia
-static void anexLista( Lista lst, TipoL elem );     // Adiciona um elemento depois do iterador
-static void insLista( Lista lst, TipoL elem );      // Adiciona um elemento antes do iterador
-static void elimLista( Lista lst );                 // Elimina o elemento que está sob o iterador
-static void primLista( Lista lst );                 // Coloca o iterador sobre o primeiro elemento da lista
-static void ultLista( Lista lst );                  // Coloca o iterador sobre o ultimo elemento da lista
-static void segLista( Lista lst );                  // Avança o iterador uma posição
-static void posLista( Lista lts, int pos );         // Coloca o iterador sobre a posição pos
-static TipoL infoLista( Lista lst );                // Retorna o elemento sob o iterador
-static int longLista( Lista lst );                  // Retorna a quantidade de elementos da lista
-static int fimLista( Lista lst );                   // Retorna verdadeiro se o iterador estiver indefinido
+static void _limpaLista( Lista lst );                   // Limpa atributos da lista
+static pListaNo _inicNo( TipoL elem );                  // Cria e retornar um novo nó com o elemento
+static void _insNoLista (Lista lst, pListaNo novoNo);   // Adiciona novo nó em uma lista vazia
+static Lista inicList( void );                          // Cria e retorna uma lista vazia
+static void anexLista( Lista lst, TipoL elem );         // Adiciona um elemento depois do iterador
+static void insLista( Lista lst, TipoL elem );          // Adiciona um elemento antes do iterador
+static void elimLista( Lista lst );                     // Elimina o elemento que está sob o iterador
+static void primLista( Lista lst );                     // Coloca o iterador sobre o primeiro elemento da lista
+static void ultLista( Lista lst );                      // Coloca o iterador sobre o ultimo elemento da lista
+static void segLista( Lista lst );                      // Avança o iterador uma posição
+static void posLista( Lista lts, int pos );             // Coloca o iterador sobre a posição pos
+static TipoL infoLista( Lista lst );                    // Retorna o elemento sob o iterador
+static int longLista( Lista lst );                      // Retorna a quantidade de elementos da lista
+static int fimLista( Lista lst );                       // Retorna verdadeiro se o iterador estiver indefinido
 
 #endif
--- src/lista.c
+++ src/lista.c
@@ -2,6 +2,31 @@
 #include "../includes/lista.h"
 #endif
 
+static void 
+_limpaLista( Lista lst ) {
+    lst -> longitude    = 0;
+    lst -> primeiro     = NULL;
+    lst -> ultimo       = NULL;
+    lst -> iterador     = NULL;
+}
+
+static pListaNo
+_inicNo( TipoL elem ) {
+    pListaNo novoNo = ( pListaNo )malloc( sizeof( struct ListaNo ) ) ;
+    novoNo -> info = elem;
+    novoNo -> prox = NULL;
+    
+    return novoNo;
+}
+
+static void
+_insNoLista (Lista lst, pListaNo novoNo) {
+    lst -> primeiro = novoNo;
+    lst -> ultimo   = novoNo;
+    lst -> iterador = novoNo;
+    lst -> longitude++;
+}
+
 static Lista
 inicList() {
     Lista lst           = ( Lista )malloc( sizeof( TLista ) );
@@ -15,16 +40,10 @@ inicList() {
 
 static void
 anexLista( Lista lst, TipoL elem ) {
-    pListaNo novoNo = ( pListaNo )malloc( sizeof( struct ListaNo ) ) ;
-
-    novoNo->info = elem;
-    novoNo->prox = NULL;
+    pListaNo novoNo = _inicNo( elem );
 
     if( lst -> longitude == 0  ) {
-        lst -> primeiro = novoNo;
-        lst -> ultimo   = novoNo;
-        lst -> iterador = novoNo;
-        lst -> longitude++;
+        _insNoLista(lst, novoNo);
     } else {
         if ( lst -> iterador == lst -> ultimo ) {
             lst -> iterador -> prox = novoNo;
@@ -38,5 +57,85 @@ anexLista( Lista lst, TipoL elem ) {
             lst -> longitude++;
         }
     }
+}
 
+static void 
+insLista( Lista lst, TipoL elem ) {
+    
+    pListaNo novoNo = _inicNo( elem );
+    pListaNo aux;
+    
+    if( lst -> longitude == 0  ) {
+        _insNoLista(lst, novoNo);
+    } else {
+        aux                 = lst -> iterador;
+        lst -> iterador     = novoNo;
+        novoNo -> prox      = aux;
+        lst -> longitude++;
+        if (lst -> primeiro == lst -> iterador) {
+            lst -> primeiro = novoNo;
+        }
+    }
+    
+}
+
+static void 
+elimLista( Lista lst ) {
+    if (lst -> longitude == 1) {
+        _limpaLista( lst );
+    } else {
+        if (lst -> iterador == lst -> ultimo) {
+            lst -> ultimo   = lst -> iterador--; //MAYBE THIS DON'T EVEN WORK
+            lst -> iterador = lst -> ultimo;
+        } else {
+            lst -> iterador-- -> prox = lst -> iterador -> prox; //MAYBE THIS DON'T EVEN WORK
+            lst -> iterador           = lst -> iterador -> prox;
+        }
+        lst -> longitude--;
+    }
+}
+
+static void 
+primLista( Lista lst ) {
+    if( lst -> longitude > 0 ) 
+        lst -> iterador = lst -> primeiro;
+}
+
+static void
+ultLista( Lista lst ) {
+    if( lst -> longitude > 0 ) 
+        lst -> iterador = lst -> ultimo;
+}
+
+static void 
+segLista( Lista lst ) {
+    if ( lst -> iterador != NULL ) {
+        lst -> iterador++;                //MAYBE THIS DON'T EVEN WORK
+    }
+}
+
+static void
+posLista( Lista lst, int pos ) {
+    if ( 1 > pos > lst -> longitude  ) {
+        lst -> iterador = NULL;
+    } else {
+        lst -> iterador = lst -> primeiro + pos;
+    }
+}
+
+static TipoL
+infoLista( Lista lst ) {
+    if ( lst -> iterador != NULL )
+        return lst -> iterador -> info;
+}
+
+static int 
+longLista( Lista lst ) {
+    return lst -> longitude;
+}
+
+static int fimLista( Lista lst ) {
+    if ( lst -> iterador == NULL ) 
+        return 1;
+    return 0;
 }
